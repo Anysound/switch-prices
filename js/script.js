93f9b729df641c176a93747da295f9f10b44eba3
Anysound <anysoundspb@gmail.com>
2018-03-12 01:49:57 +0300

    test task

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+            $('.stepper-arrow.down').click(function () {
+                var $input = $(this).parent().find('input');
+                var count = parseInt($input.val()) - 1;
+                count = count < 1 ? 1 : count;
+                $input.val(count);
+                $input.change();
+                return false;
+            });
+            $('.stepper-arrow.up').click(function () {
+                var $input = $(this).parent().find('input');
+                $input.val(parseInt($input.val()) + 1);
+                $input.change();
+                return false;
+            });
+        });
+
+
---
+++ js/script2.js
@@ -0,0 +1,98 @@
+$('.tab-link').click(function(event) {
+    event.preventDefault();
+    $('.tab-link').removeClass('tab-link-active');
+    $(this).addClass('tab-link-active');
+    var blockId = $(this).data('id');
+    $('.tab-block').removeClass('tab-block-active');
+    $('#' + blockId).addClass('tab-block-active');
+});
+
+$('.tab-link-1').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-1').removeClass('tab-link-active-1');
+    $(this).addClass('tab-link-active-1');
+    var blockId = $(this).data('id');
+    $('.tab-block-1').removeClass('tab-block-active-1');
+    $('#' + blockId).addClass('tab-block-active-1');
+});
+
+$('.tab-link-2').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-2').removeClass('tab-link-active-2');
+    $(this).addClass('tab-link-active-2');
+    var blockId = $(this).data('id');
+    $('.tab-block-2').removeClass('tab-block-active-2');
+    $('#' + blockId).addClass('tab-block-active-2');
+});
+
+$('.tab-link-3').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-3').removeClass('tab-link-active-3');
+    $(this).addClass('tab-link-active-3');
+    var blockId = $(this).data('id');
+    $('.tab-block-3').removeClass('tab-block-active-3');
+    $('#' + blockId).addClass('tab-block-active-3');
+});
+
+$('.tab-link-4').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-4').removeClass('tab-link-active-4');
+    $(this).addClass('tab-link-active-4');
+    var blockId = $(this).data('id');
+    $('.tab-block-4').removeClass('tab-block-active-4');
+    $('#' + blockId).addClass('tab-block-active-4');
+});
+
+$('.tab-link-5').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-5').removeClass('tab-link-active-5');
+    $(this).addClass('tab-link-active-5');
+    var blockId = $(this).data('id');
+    $('.tab-block-5').removeClass('tab-block-active-5');
+    $('#' + blockId).addClass('tab-block-active-5');
+});
+
+$('.tab-link-6').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-6').removeClass('tab-link-active-6');
+    $(this).addClass('tab-link-active-6');
+    var blockId = $(this).data('id');
+    $('.tab-block-6').removeClass('tab-block-active-6');
+    $('#' + blockId).addClass('tab-block-active-6');
+});
+
+$('.tab-link-7').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-7').removeClass('tab-link-active-7');
+    $(this).addClass('tab-link-active-7');
+    var blockId = $(this).data('id');
+    $('.tab-block-7').removeClass('tab-block-active-7');
+    $('#' + blockId).addClass('tab-block-active-7');
+});
+
+$('.tab-link-8').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-8').removeClass('tab-link-active-8');
+    $(this).addClass('tab-link-active-8');
+    var blockId = $(this).data('id');
+    $('.tab-block-8').removeClass('tab-block-active-8');
+    $('#' + blockId).addClass('tab-block-active-8');
+});
+
+$('.tab-link-9').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-9').removeClass('tab-link-active-9');
+    $(this).addClass('tab-link-active-9');
+    var blockId = $(this).data('id');
+    $('.tab-block-9').removeClass('tab-block-active-9');
+    $('#' + blockId).addClass('tab-block-active-9');
+});
+
+$('.tab-link-10').click(function(event) {
+    event.preventDefault();
+    $('.tab-link-10').removeClass('tab-link-active-10');
+    $(this).addClass('tab-link-active-10');
+    var blockId = $(this).data('id');
+    $('.tab-block-10').removeClass('tab-block-active-10');
+    $('#' + blockId).addClass('tab-block-active-10');
+});
